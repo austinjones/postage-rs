4c7828036863d5067ec498e8b02022adcb3856f6
Austin Jones <implAustin@gmail.com>
2021-01-15 23:12:43 -0500

    Lib docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
-//! The feature-rich, portable async channel library.
+//! The feature-rich, portable async channel library. Provides a set of async channels that can be used with any async executor.
 //!
-//! Provides a set of async channels that can be used with any async executor.
+//! Why use Postage?
+//! - Includes a **rich set of channels**, and it works with any async executor (currently regressions are written for tokio and async-std)
+//!   - `mpsc`, a multi-producer, single-consumer channel
+//!   - `broadcast`, a multi-producer, multi-consumer broadcast channel with backpressure (no lagging!)
+//!   - `watch`, a stateful channel where receivers receive an initial value, and updates when the value state changes.
+//!   - `oneshot`, a transfer channel that can be used once.
+//!   - `barrier`, a channel that doesn't carry a value, but transmits when the sender half is dropped.
+//! - Comes with **built-in Stream and Sink combinators**.
+//!   - Sinks can be chained, and filtered.
+//!   - Streams can be chained, filtered, mapped, and merged.
+//!   - Sinks and streams can log their values, for easy app debugging.
+//!
+//! Postage is in *beta* quality.  The functionality is implemented and tested, but needs to be tested on more hardware, and more operating systems.
+
 mod channels;
 mod sink;
 mod stream;
